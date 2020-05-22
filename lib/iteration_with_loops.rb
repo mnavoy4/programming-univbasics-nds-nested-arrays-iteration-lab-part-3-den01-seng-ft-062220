552b52536c46acd7d77fc3a07170833433b26d64
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-22 14:08:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,13 @@ def join_nested_strings(src)
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do
-      if src[row_index][element_index].class == 
+      if src[row_index][element_index].class == String
+        string += src[row_index][element_index]
+      end
+      element_index += 1 
+    end
+    row_index += 1 
+  end
+  string
   
 end
